623c4387e6ba90b62ae99d961c052cf336b78be7
XBagon <xbagon@outlook.de>
2023-09-19 06:32:37 +0200

    fixed snapshot tests

--- tests/snapshot_tests.rs
+++ tests/snapshot_tests.rs
@@ -7,7 +7,7 @@ use test_case::test_matrix;
 #[test_matrix(
     0..100
 )]
-pub fn random_board_snapshots(seed: u64) {
+pub fn random_board_snapshot(seed: u64) {
     let mut game = Game::with_seed(seed);
 
     let mut rng = Xoshiro256PlusPlus::seed_from_u64(!seed);
@@ -24,28 +24,5 @@ pub fn random_board_snapshots(seed: u64) {
 
     let recording = game.run_and_record_events();
 
-    assert_yaml_snapshot!(recording);
+    assert_yaml_snapshot!(format!("random_board_snapshot_{}", seed), recording);
 }
-
-// just to test out reproducability of snapshot tests
-//
-//#[test_matrix(
-//    0..100
-//)]
-//pub fn random_board(seed: u64) {
-//    let mut game = Game::with_seed(seed);
-//
-//    let mut rng = Xoshiro256PlusPlus::seed_from_u64(!seed);
-//
-//    for _ in 0..7 {
-//        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
-//        game.position_minion(minion, PlayerId::Bottom).unwrap();
-//
-//        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
-//        game.position_minion(minion, PlayerId::Top).unwrap();
-//    }
-//
-//    game.initialize();
-//
-//    game.run_and_print();
-//}
