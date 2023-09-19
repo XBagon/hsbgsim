2f236846ebc5913e6c7a5e679497849b3ee86234
XBagon <xbagon@outlook.de>
2023-09-19 06:19:30 +0200

    oops

--- tests/snapshot_tests.rs
+++ tests/snapshot_tests.rs
@@ -13,10 +13,10 @@ pub fn random_board_snapshots(seed: u64) {
     let mut rng = Xoshiro256PlusPlus::seed_from_u64(!seed);
 
     for _ in 0..7 {
-        let minion = game.instantiate_minion(MinionVariant::Bonker);
+        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
         game.position_minion(minion, PlayerId::Bottom).unwrap();
 
-        let minion = game.instantiate_minion(MinionVariant::Bonker);
+        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
         game.position_minion(minion, PlayerId::Top).unwrap();
     }
 
@@ -27,23 +27,25 @@ pub fn random_board_snapshots(seed: u64) {
     assert_yaml_snapshot!(recording);
 }
 
-#[test_matrix(
-    0..100
-)]
-pub fn random_board(seed: u64) {
-    let mut game = Game::with_seed(seed);
-
-    let mut rng = Xoshiro256PlusPlus::seed_from_u64(!seed);
-
-    for _ in 0..7 {
-        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
-        game.position_minion(minion, PlayerId::Bottom).unwrap();
-
-        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
-        game.position_minion(minion, PlayerId::Top).unwrap();
-    }
-
-    game.initialize();
-
-    game.run_and_print();
-}
+// just to test out reproducability of snapshot tests
+//
+//#[test_matrix(
+//    0..100
+//)]
+//pub fn random_board(seed: u64) {
+//    let mut game = Game::with_seed(seed);
+//
+//    let mut rng = Xoshiro256PlusPlus::seed_from_u64(!seed);
+//
+//    for _ in 0..7 {
+//        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
+//        game.position_minion(minion, PlayerId::Bottom).unwrap();
+//
+//        let minion = game.instantiate_minion(MinionVariant::random(&mut rng));
+//        game.position_minion(minion, PlayerId::Top).unwrap();
+//    }
+//
+//    game.initialize();
+//
+//    game.run_and_print();
+//}
