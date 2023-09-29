37b7c1173433fa5c844dbe3b638fbee969c3e8ee
XBagon <xbagon@outlook.de>
2023-09-29 04:42:59 +0200

    implemented HarvestGolem

--- src/minions/variants/harvest_golem.rs
+++ src/minions/variants/harvest_golem.rs
@@ -1,4 +1,14 @@
-use crate::events::EventHandlers;
+use crate::events::{EventHandlers, Summon};
+
 pub fn event_handlers() -> EventHandlers {
-    EventHandlers::default()
+    EventHandlers {
+        death: Some(|this, death, game| {
+            if death.minion == this {
+                let position = game.minion_instances.get(this).unwrap().position;
+                let damaged_golem = game.instantiate_minion(crate::MinionVariant::DamagedGolem);
+                game.push_event(Summon::new(damaged_golem, *position.unwrap(), this).into());
+            }
+        }),
+        ..Default::default()
+    }
 }
