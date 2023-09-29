c2c0e2de02c6f93abff1edb5bfbb42ed09c0c197
XBagon <xbagon@outlook.de>
2023-09-29 04:24:09 +0200

    27.4.2 Patch BGJson id of ImposingPercussionist was moved, so most snapshots have different minions in them

--- src/minions/variants/data/imposing_percussionist.rs
+++ src/minions/variants/data/imposing_percussionist.rs
@@ -3,8 +3,8 @@ use crate::minions::AbilitiesInit;
 pub fn data() -> MinionVariantData {
     MinionVariantData {
         name: String::from("Imposing Percussionist"),
-        health: 6u8,
-        attack: 6u8,
+        health: 4u8,
+        attack: 4u8,
         abilities: AbilitiesInit {
             battlecry: true,
             ..Default::default()
