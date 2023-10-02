a0b30ea7a7b5502a632f0a040c9825b1263e25c6
XBagon <xbagon@outlook.de>
2023-10-02 06:05:34 +0200

    add back todos I accidentially nuked

--- src/minions/variants/ghastcoiler.rs
+++ src/minions/variants/ghastcoiler.rs
@@ -1,4 +1,6 @@
 use crate::events::EventHandlers;
+
+//TODO: implement minion lists
 pub fn event_handlers() -> EventHandlers {
     EventHandlers::default()
 }
--- src/minions/variants/ghoul_of_the_feast.rs
+++ src/minions/variants/ghoul_of_the_feast.rs
@@ -1,4 +1,6 @@
 use crate::events::EventHandlers;
+
+//TODO: implement minion state (avenge status) and do minion type queries
 pub fn event_handlers() -> EventHandlers {
     EventHandlers::default()
 }
